f326d4607960f1ef07b26ad402ea348ef61414c6
Brittney Kistner <bkistner01@gmail.com>
2014-07-16 10:08:17 -0700

    Working on change method

--- day3/day3.rb
+++ day3/day3.rb
@@ -8,3 +8,52 @@ def time_for_milk_and_cookies(d)
     false
   end
 end
+
+# Given an array of numbers return an array of numbers from the array that qualify as perfect squares. A perfect square is defined as a whole number that, when square rooted, is a whole number. (Such as 1, 4, 9, 16, etc, etc.)
+
+# Note: Return only one copy of each perfect square in ascending order
+
+def get_squares(array)
+  array.map! do |x|
+    if (x**(1/2)).class == Fixnum
+      x
+    end
+  end
+  array.compact.uniq.sort!
+end
+
+# This function has to be called multiply and needs to take two numbers as arguments, and has to return the multiplication of the two arguments.
+def multiply(x,y)
+  x*y
+end
+
+# Add the value "codewars" to the websites array.
+# After your code executes the websites array should == ["codewars"]
+websites << "codewars"
+
+# String search
+
+# Create a method to accept one arbitrary string as an argument, and return a string of length 26.
+
+# The objective is to detect if letters of the alphabet (upper or lower case) are present anywhere in the string, and to set each of the 26 characters corresponding to each letter to either '1' if present, '0' if not.
+
+# So if an 'a' or an 'A' appears anywhere in the argument string (any number of times), set the first character of the returned string to '1' otherwise to '0', if 'b' or 'B' the the second to '1', and so on for the rest of the alphabet.
+
+# So the returned string consists only of '1's and '0's.
+
+# For instance
+
+# change('a   **&  bZ') =>  '11000000000000000000000001'
+
+def change(str)
+  if str.class == String
+    #convert to an array and then iterate through array
+    array = str.split(//)
+    array.uniq.sort!
+    array.each do |x|
+      # if /[[:alpha:]]/.match(x)
+
+
+end
+
+
--- day3/spec/day3_spec.rb
+++ day3/spec/day3_spec.rb
@@ -5,3 +5,11 @@ describe 'time_for_milk_and_cookies' do
     expect(time_for_milk_and_cookies(Date.new(2001, 12, 24))).to eq(true)
   end
 end
+
+describe 'get_squares' do
+  xit 'receives an array of elements, checks for elements which are a perfect square and returns those elements in an array sorted in ascending order with no duplicates' do
+    expect(get_squares([3,6,9,4,25,16,4])).to eq([4,9,16,25])
+  end
+end
+
+
