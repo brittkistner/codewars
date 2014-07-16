5a0e46a64485a7b646d40d70b8c0ffe087e78581
Brittney Kistner <bkistner01@gmail.com>
2014-07-16 08:57:18 -0700

    Corrected the authenticate method for day2 problems

--- day2/day2.rb
+++ day2/day2.rb
@@ -37,7 +37,7 @@ end
 # Your task is to implement the authenticate() method of the sleigh, which takes the name of the person, who wants to board the sleigh and a secret password. If, and only if, the name equals "Santa Claus" and the password is "Ho Ho Ho!" (yes, even Santa has a secret password with uppercase and lowercase letters and special characters :D), the return value must be true. Otherwise it should return false.
 
 def authenticate(name, password)
-  if name != "Santa Claus" && password != "Ho Ho Ho!"
+  if name != "Santa Claus" || password != "Ho Ho Ho!"
     return false
   end
   true
