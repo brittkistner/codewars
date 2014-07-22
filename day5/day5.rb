2fc900b53d7b2a3efa2b5393550338d91d38be19
Brittney Kistner <bkistner01@gmail.com>
2014-07-22 10:59:43 -0700

    completed day5 codewars problems

---
+++ day5/day5.rb
@@ -0,0 +1,84 @@
+# Make a simple function called greet that returns the most-famous "hello world!".
+
+# Although this has been thought as a simple exercise for beginners, other people chimed in and loved to show off how wonderfully complicated you can write "hello world!" - so go for it, create the most beautiful "hello world!"
+
+def greet
+  'hello world!'
+end
+###################
+
+# Just a simple sorting usage. Create a function that returns the elements of the input-array in a sorted manner.
+def sortme(names)
+  names.sort
+end
+###################
+# There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
+
+class MrFreeze
+  def self.frozen
+    MrFreeze.freeze
+  end
+end
+
+###################
+# Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+
+# Example:
+
+# solution(5) # should return "Value is 00005"
+
+def solution(value)
+  answer = ""
+  number_string = value.to_s
+  ( 5 - number_string.length).times do
+         answer += "0"
+   end
+   answer = answer + number_string
+    "Value is #{answer}"
+end
+
+# Alternative:
+
+# def solution(value)
+#   "Value is %.5d" % value
+# end
+#####################
+
+# Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+# For example:
+
+# solution([1, 2, 10, 50, 5]) # should return [1,2,5,10,50]
+# solution(nil) # should return []
+def sort_array(nums)
+  nums.nil? ? [] : nums.sort
+end
+
+# Alternative
+# def solution(nums)
+#   Array(nums).sort
+# end
+#####################
+# Complete the solution so that it sorts an array of hashes passed in by the hash key specified.
+
+# Example:
+
+# solution([{'a' => 2}, {'a' => 1}], 'a')
+# # => [{'a' => 1}, {'a' => 2}]]
+
+# solution([{a: 1}, {a: 5}, {a: 3}], :a)
+# # => [{a:1}, {a: 3}, {a: 5}]
+
+# def solution(array, key)
+#   array.map! do |x|
+#     if x.keys == key
+#       x
+#     end
+#   end
+#   array.map! do |x|
+#     if x
+#       #sort???
+# end
+
+
+
+
---
+++ day5/spec/day5_spec.rb
@@ -0,0 +1,33 @@
+require './day5.rb'
+
+describe 'greet' do
+  it 'returns the string hello world' do
+    expect(greet).to eq("hello world!")
+  end
+end
+
+describe 'sortme' do
+  it 'sorts an array of strings' do
+    expect(sortme(["suzie", "alinna", "ben"])).to eq(["alinna", "ben", "suzie"])
+  end
+end
+
+describe 'frozen' do
+  it 'applies the freeze method to the MrFreeze class' do
+    MrFreeze.frozen
+    expect(MrFreeze.frozen?).to eq(true)
+  end
+end
+
+describe 'solution' do
+  it 'returns a 5 digit padded number given a value' do
+    expect(solution(50)).to eq("Value is 00050")
+  end
+end
+
+describe 'sort_array' do
+  it 'sorts an array or returns an empty array if nil' do
+    expect(sort_array(nil)).to eq([])
+    expect(sort_array([40,1,50])).to eq([1,40,50])
+  end
+end
