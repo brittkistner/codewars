26277f96e71b4da39de9c54ee59738d758a5a7dd
Brittney Kistner <bkistner01@gmail.com>
2014-07-31 11:13:39 -0700

    Completed day7 problems

--- day7/day7.rb
+++ day7/day7.rb
@@ -31,11 +31,111 @@ end
 # title_case('THE WIND IN THE WILLOWS', 'The In') # should return: 'The Wind in the Willows'
 # title_case('the quick brown fox') # should return: 'The Quick Brown Fox'
 
-def title_case(title, minor_words)
-array_title = title.downcase.split(" ")
-array_minor = minor_words.downcase.split(" ")
+def title_case(title, *minor_words)
+  array_title = title.downcase.split(" ")
+  if minor_words.empty?
+    array_title.map! {|word| word.capitalize}
+    return array_title.join(" ")
+  end
 
-array_title.map!.with_index {|word, i| i == 0 ? word.capitalize : (array_minor.include?(word) ? word : word.capitalize)}
+  array_minor = minor_words[0].downcase.split(" ")
 
-array_title.join(" ")
+  array_title.map!.with_index {|word, i| i == 0 ? word.capitalize : (array_minor.include?(word) ? word : word.capitalize)}
+
+  array_title.join(" ")
 end
+
+# Create a method/function that returns an array of numbers from 1 to 100.
+
+# If no arguments are passed the value at the index of the array for number should be 'Fizz' if it is divisible by 3, 'Buzz' if divisible by 5, 'FizzBuzz' if it divisible by both 3 and 5, or the number if it is not divisible by 3 or 5.
+
+# The method should take up to 4 arguments:
+
+# The first and second arguments are strings, which should be 'Fizz' and 'Buzz' by default.
+
+# The third and fourth arguments are integers and and should be 3 and 5 by default.
+
+# Example
+
+# fizz_buzz_custom[15]                         # returns 16
+# fizz_buzz_custom[44]                         # returns "FizzBuzz" (45 is divisible by 3 and 5)
+# fizz_buzz_custom('Hey', 'There')[25]         # returns 26
+# fizz_buzz_custom('Hey', 'There')[11]         # returns "Hey" (12 is divisible by 3)
+# fizz_buzz_custom("What's ", "up?", 3, 7)[80] # returns "What's " (81 is divisible by 3)
+
+
+def fizz_buzz_custom(string_1 = "Fizz", string_2 = "Buzz", num_1 = 3, num_2 = 5)
+fizz_buzz_custom = (1..100).to_a
+fizz_buzz_custom.map! do |num|
+  if (num % num_1) == 0 && (num % num_2) == 0
+    string_1+string_2
+  elsif (num % num_1) == 0
+    string_1
+  elsif (num % num_2) == 0
+    string_2
+  else
+    num
+  end
+end
+fizz_buzz_custom
+end
+##################
+# You are the greatest chef on earth. No one boils eggs like you! Your restaurant is always full of guests, who love your boiled eggs. But when there is a greater order of boiled eggs, you need some time, because you have only one pot for your job. How much time do you need?
+
+# Your Task
+
+# Implement a function, which takes a non-negative integer, representing the number of eggs to boil. It must return the time in minutes (integer), which it takes to have all the eggs boiled.
+
+# Rules
+
+# you can put at most 8 eggs into the pot at once
+# it takes 5 minutes to boil an egg
+# we assume, that the water is boiling all the time (no time to heat up)
+# for simplicity we also don't consider the time it takes to put eggs into the pot or get them out of it
+# Example
+
+# cooking_time 0 # must return 0
+# cooking_time 5 # must return 5
+# cooking_time 10 # must return 10
+
+
+def cooking_time(eggs)
+  eggs <= 0 ? 0 : ((eggs.to_f/8).ceil)*5
+end
+
+
+# Complete the Compute method/function so that if a block is given it will run else it returns "Do not compute".
+def compute
+  block_given? ? yield : "Do not compute"
+end
+###############
+
+# Complete the solution so that it takes the object (JavaScript/CoffeeScript) or hash (ruby) passed in and generates a human readable string from its key/value pairs.
+
+# The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
+
+# Example:
+
+# solution({a: 1, b: '2'}) # should return "a = 1,b = 2"
+
+def solution(pairs)
+  pairs.to_a.map {|x| x.join(" = ")}.join(",")
+end
+
+#Alternative
+# def solution(pairs)
+#   # TODO: complete
+#   pairs.map{|k,v| "#{k} = #{v}"}.join(',')
+# end
+
+
+
+
+
+
+
+
+
+
+
+
--- day7/spec/day7_spec.rb
+++ day7/spec/day7_spec.rb
@@ -10,5 +10,14 @@ end
 describe 'title_case' do
   it 'converts a string into title case' do
     expect(title_case('a clash of KINGS', 'a an the of')).to eq('A Clash of Kings')
+    expect(title_case('the quick brown fox')).to eq("The Quick Brown Fox")
+  end
+end
+
+describe 'fizz_buzz_custom' do
+  it 'returns an array of 100 elements with either a number (1-100), Fizz, Buzz, or FizzBuzz' do
+    expect(fizz_buzz_custom()).to be_a(Array)
+    expect(fizz_buzz_custom[44]).to eq("FizzBuzz")
+    expect(fizz_buzz_custom("What's ", "up?", 3, 7)[80]).to eq("What's ")
   end
 end
